e275e5614ae82169f3a1c4af3df46a1d9d902815
Anurag Swarnakar <anuragswarnakar626@gmail.com>
2026-07-05 19:05:58 +0530

    refactor: use application database settings in Alembic

--- alembic/env.py
+++ alembic/env.py
@@ -7,9 +7,16 @@ from alembic import context
 
 from app.models import Base  # type: ignore
 
+from app.config import settings
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+config.set_main_option(
+    "sqlalchemy.url",
+    settings.DATABASE_URL
+)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -21,11 +28,6 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
 
 
 def run_migrations_offline() -> None:
